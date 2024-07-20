8104c25fe96573eb2df23e4fe2e5d6231d2e60fa
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-07-20 11:36:24 -0500

    footer

--- front-portafolio/src/pages/check-role/index.tsx
+++ front-portafolio/src/pages/check-role/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Navbar from '@/components/Navbar/Navbar';
-import Footer from '@/components/Footer';
+import Footer from '@/components/Footer/Footer';
 
 const CheckRole = () => {
   const [role, setRole] = useState('');
