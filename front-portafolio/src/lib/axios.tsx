8712892b831ebe27be5991c6a53de8abcec3096b
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-07-26 11:27:46 -0500

    cookies

--- front-portafolio/src/lib/axios.tsx
+++ front-portafolio/src/lib/axios.tsx
@@ -1,7 +1,7 @@
-import axios, { AxiosInstance, AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const instance: AxiosInstance = axios.create({
+const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
   headers: {
@@ -10,40 +10,22 @@ const instance: AxiosInstance = axios.create({
   },
 });
 
-let isRefreshing = false;
-let refreshPromise: Promise<AxiosResponse> | null = null;
-
-const refreshCSRFToken = async () => {
-  if (!isRefreshing) {
-    isRefreshing = true;
-    refreshPromise = instance.get('/sanctum/csrf-cookie').finally(() => {
-      isRefreshing = false;
-    });
+instance.interceptors.request.use(async config => {
+  const token = Cookies.get('XSRF-TOKEN');
+  if (!token) {
+    await instance.get('/sanctum/csrf-cookie');
   }
-  return refreshPromise;
-};
-
-instance.interceptors.request.use(
-  async (config: InternalAxiosRequestConfig) => {
-    let token = Cookies.get('XSRF-TOKEN');
-    if (!token) {
-      await refreshCSRFToken();
-      token = Cookies.get('XSRF-TOKEN');
-    }
-    config.headers['X-XSRF-TOKEN'] = token;
-    return config;
-  },
-  (error: AxiosError) => Promise.reject(error)
-);
-
+  config.headers['X-XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN');
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
 instance.interceptors.response.use(
   response => response,
-  async (error: AxiosError) => {
-    const originalRequest = error.config as InternalAxiosRequestConfig & { _retry?: boolean };
-    if (error.response?.status === 419 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      await refreshCSRFToken();
-      return instance(originalRequest);
+  async error => {
+    if (error.response && error.response.status === 419) {
+      await instance.get('/sanctum/csrf-cookie');
+      return instance(error.config);
     }
     return Promise.reject(error);
   }
