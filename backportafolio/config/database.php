8b480e081a5a7665a4d2783648326dbb3dbdae17
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-07-26 09:30:56 -0500

    docker

--- backportafolio/config/database.php
+++ backportafolio/config/database.php
@@ -79,18 +79,18 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
+'pgsql' => [
+    'driver' => 'pgsql',
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'schema' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-        ],
+],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
