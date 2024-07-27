6ef511da6976295664437c02f06b50acbfbfc604
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-07-27 13:45:48 -0500

    fhg

--- backportafolio/config/sanctum.php
+++ backportafolio/config/sanctum.php
@@ -77,12 +77,12 @@ return [
     |
     */
 
+
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
         'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
-    'same_site' => 'none',
-
+    'same_site' => env('SESSION_SAME_SITE', 'none'),
 ];
