e8dd1202c36a5c820518e37486f8bd583c083222
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-07-26 20:09:16 -0500

    czx

--- front-portafolio/src/lib/axios.tsx
+++ front-portafolio/src/lib/axios.tsx
@@ -4,12 +4,15 @@ import Cookies from 'js-cookie';
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
+    
     'X-Requested-With': 'XMLHttpRequest',
-    'Accept': 'application/json',
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
   },
   withCredentials: true,
-});
+}
+
+);
 
 instance.interceptors.request.use(
   config => {
