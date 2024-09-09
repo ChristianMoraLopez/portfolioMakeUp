44f958bc4ac28f105afd9969aae1b16eaa7f75a8
Christian Mora <christianmoralopez@hotmail.com>
2024-09-09 15:33:48 -0500

    cors catre doble

--- backportafolio/bootstrap/app.php
+++ backportafolio/bootstrap/app.php
@@ -24,6 +24,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '*',
             'api/*',
+            'services/*',
+            'services/*/*',
+            'services',
             'sanctum/csrf-cookie',
             'logout',
             'login',
