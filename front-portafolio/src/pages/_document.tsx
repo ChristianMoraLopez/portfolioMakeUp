a7c78554618f4a379e6051234d191c79dd423a5e
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-09-23 15:25:03 -0500

    logo

--- front-portafolio/src/pages/_document.tsx
+++ front-portafolio/src/pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Ms+Madi&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/images/LogoMakeUpBlackAndWhite.svg" type="image/svg" />
         </Head>
         <body>
           <Main />
